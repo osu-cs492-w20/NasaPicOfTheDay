ab0c11fa2f2b07b3b8a11f7a188901b58109ddd9
Brian Kim <kimbri@oregonstate.edu>
2020-03-14 13:22:06 -0700

    added json

---
+++ app/src/main/java/com/example/android/nasapicoftheday/utils/PicList.java
@@ -0,0 +1,11 @@
+package com.example.android.nasapicoftheday.utils;
+
+import java.io.Serializable;
+
+public class PicList implements Serializable {
+    public String date;
+    public String explanation;
+    public String hdurl;
+    public String title;
+    public String url;
+}
