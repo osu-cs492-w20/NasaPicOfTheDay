997dd91d0cac99e24d6a7560403bcaceec43c030
ChibaTosh <chibat@oregonstate.edu>
2020-03-14 16:55:52 -0700

    adding feature of download images in progress

--- app/src/main/java/com/example/android/nasapicoftheday/MainActivity.java
+++ app/src/main/java/com/example/android/nasapicoftheday/MainActivity.java
@@ -1,16 +1,27 @@
 package com.example.android.nasapicoftheday;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.AsyncTask;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.example.android.nasapicoftheday.utils.NetworkUtils;
 import com.example.android.nasapicoftheday.utils.PicList;
 import com.example.android.nasapicoftheday.utils.PicOfDayUtils;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.squareup.picasso.Picasso;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
@@ -28,7 +39,30 @@ public class MainActivity extends AppCompatActivity implements PicAdapter.OnPicI
 
     private RecyclerView mPicListRV;
     private PicAdapter mPicAdapter;
+    ImageView myImageDownload;
 
+    private static final int PERMISSION_REQUEST_CODE = 1000;
+
+    Button downloadBtn;
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+            case PERMISSION_REQUEST_CODE:
+            {
+                if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    Toast.makeText(this, "Permission Granted", Toast.LENGTH_SHORT).show();
+                }
+                else {
+                    Toast.makeText(this, "Permission Denied", Toast.LENGTH_SHORT).show();
+                }
+            }
+                break;
+        }
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.M)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -43,14 +77,30 @@ public class MainActivity extends AppCompatActivity implements PicAdapter.OnPicI
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(navView, navController);
 
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            requestPermissions(new String[]{}, PERMISSION_REQUEST_CODE);
+        }
+
         //recyclerView setup
         mPicListRV = findViewById(R.id.rv_pic_list);
         mPicListRV.setLayoutManager(new LinearLayoutManager(this));
         mPicAdapter = new PicAdapter(this);
         mPicListRV.setAdapter(mPicAdapter);
 
+        downloadBtn = (Button)findViewById(R.id.downloadBtn);
+        downloadBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+                    Toast.makeText(MainActivity.this, "you should grant permission", Toast.LENGTH_SHORT).show();
+                    requestPermissions(new String[]{}, PERMISSION_REQUEST_CODE);
+                    return;
+                }
+                else {
 
-
+                }
+            }
+        });
         doPicSearch();
 
     }
