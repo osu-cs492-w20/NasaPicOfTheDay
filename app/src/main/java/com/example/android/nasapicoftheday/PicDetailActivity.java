a3253f34be40ad5453b6ca7728f597210f8bd087
ChibaTosh <chibat@oregonstate.edu>
2020-03-14 14:19:00 -0700

    please not crash

---
+++ app/src/main/java/com/example/android/nasapicoftheday/PicDetailActivity.java
@@ -0,0 +1,14 @@
+package com.example.android.nasapicoftheday;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PicDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pic_detail);
+    }
+}
